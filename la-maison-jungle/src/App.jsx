98a4c6dd7201c9ebee9355be4684660235da5aff
unknown <nouranmalidi@gmail.com>
2026-04-28 16:35:19 +0300

    initialisation a react

---
+++ la-maison-jungle/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+// J'importe le coposant Banner.jsx
+import Banner from './components/Banner.jsx'
+
+
+function App() {
+  
+  return (
+    <>
+     <Banner />
+      
+    </>
+  )
+}
+
+export default App
---
+++ la-maison-jungle/src/components/Banner.jsx
@@ -0,0 +1,19 @@
+/**
+ * Banner est le composant pour présenter la bannière (la barre de navigation)  
+ */
+
+// On défint le composant Banner qui est également une fonction avec un message h1 
+const Banner = () => {
+    return <h1>Hello bienvenue à la maison Jungle </h1>;
+}
+
+// Méthode d'openclassrooom
+// createRoot(document.getElementById('root')).render(<Banner />);
+
+
+
+
+
+
+// export du composant Banner 
+export default Banner
